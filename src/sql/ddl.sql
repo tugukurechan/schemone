08258a51bea4844c53bf95ff82a5a5f91f2a0470
tugukurechan <tugukurechan@gmail.com>
2024-09-01 22:12:09 +0900

    feature/ddl.splの作成

--- src/sql/ddl.sql
+++ src/sql/ddl.sql
@@ -1,5 +1,79 @@
+DROP TABLE if EXISTS links;
+-- リンクを管理する
+CREATE TABLE groups (
+    id text PRIMARY KEY,
+    name text NOT NULL,
+    -- all_money INTEGER DEFAULT 0
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --作成日時
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP --更新日時
+);
+
 DROP TABLE if EXISTS users;
-CREATE TABLE users(
-    id serial PRIMARY KEY,
+-- ユーザを管理する
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    group_id text REFERENCES groups(id),
     name text NOT NULL
 );
+
+DROP TABLE if EXISTS events;
+-- イベントを管理する
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    group_id text REFERENCES groups(id),
+    event_name text NOT NULL,
+    total_amount INTEGER NOT NULL, --イベント全体の合計金額
+    paid_user_id INTEGER REFERENCES users(id),-- 支払ったユーザID
+    
+    event_date DATE,
+    event_time TIME,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --作成日時
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP --更新日時
+);
+
+DROP TABLE if EXISTS event_instead_users;
+-- 誰が誰の分を立て替えたか
+CREATE TABLE event_instead_users (
+    id SERIAL PRIMARY KEY,
+    group_id text REFERENCES groups(id),
+    event_id INTEGER REFERENCES events(id),
+    user_id INTEGER REFERENCES users(id)
+);
+
+DROP TABLE if EXISTS debts;
+-- ユーザの借金を管理する
+CREATE TABLE debts (
+    id SERIAL PRIMARY KEY,
+    group_id text REFERENCES groups(id),
+    creditor_id INTEGER REFERENCES users(id),  -- 立て替えた人のID
+    debtor_id INTEGER REFERENCES users(id),    -- 借りた人のID
+    amount INTEGER NOT NULL,            -- 借金の金額
+    event_id INTEGER REFERENCES events(id),    -- どのイベントに関連するか
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- DROP TABLE if EXISTS final_debts;
+-- -- ユーザの最終的な借金を管理する
+-- CREATE TABLE final_debts (
+--     id SERIAL PRIMARY KEY,
+--     creditor_id INTEGER REFERENCES users(id),  -- 立て替えた人のID
+--     debtor_id INTEGER REFERENCES users(id),    -- 借りた人のID
+--     final_amount INTEGER NOT NULL,      -- 最終的な精算金額
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- 1イベントの作成:
+-- 新しいイベントが作成され、events テーブルに保存されます。
+-- 支払った人のIDが paid_user_id に保存され、イベントの合計金額が total_amount に保存されます。
+
+-- 2立替情報の管理:
+-- イベントに参加した各ユーザの立て替え情報が event_instead_users テーブルに保存されます。
+-- 立て替えられた各ユーザに対して、share_amount の金額が保存されます。
+
+-- 3負債の計算:
+-- debts テーブルに、各ユーザ間の負債関係が保存されます。
+-- 支払ったユーザ（creditor_id）と立て替えられたユーザ（debtor_id）の情報が保存され、金額が amount に記録されます。
